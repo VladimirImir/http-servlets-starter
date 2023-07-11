6669d0e35f4cc6ecc073a16bfb78961c3e299537
VladimirImir <mimimvvv@gmail.com>
2023-07-11 22:47:12 +0300

    HTTP. Servlets. 28. - Работа с заголовками. HTTP Headers.

--- src/com/dev/http/servlet/FirstServlet.java
+++ src/com/dev/http/servlet/FirstServlet.java
@@ -9,6 +9,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
 
 @WebServlet("/first")
 public class FirstServlet extends HttpServlet {
@@ -25,9 +27,18 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
+        //req.getHeader("user-agent");
+        var headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            var header = headerNames.nextElement();
+            System.out.println(req.getHeader(header));
+        }
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setHeader("token", "12345");
+        //resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
         try (var writer = resp.getWriter()) {
-            writer.write("Hello from First Servlet.");
+            writer.write("Hello from First Servlet. Привет с первого сервлета!");
         }
     }
 
