697f7c0db69f1531f2bc1aecc16cb3e5ba5fcf93
VladimirImir <mimimvvv@gmail.com>
2023-07-17 21:45:14 +0300

    HTTP. Servlets. 32. - Архитектура web приложений. MVC.

--- src/com/dev/http/servlet/DownloadServlet.java
+++ src/com/dev/http/servlet/DownloadServlet.java
@@ -15,6 +15,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+/** HTTP. Servlets. 32. Архитектура web приложений. MVC */
+
 @WebServlet("/download")
 public class DownloadServlet extends HttpServlet {
 
