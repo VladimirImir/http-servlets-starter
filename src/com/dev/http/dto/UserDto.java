a376b8df6c36f00d2563c20bc151e32c3c785182
VladimirImir <mimimvvv@gmail.com>
2023-08-01 14:26:34 +0300

    HTTP. Servlets. 37. - Attributes.

---
+++ src/com/dev/http/dto/UserDto.java
@@ -0,0 +1,11 @@
+package com.dev.http.dto;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class UserDto {
+    Long id;
+    String mail;
+}
--- src/com/dev/http/servlet/SessionServlet.java
+++ src/com/dev/http/servlet/SessionServlet.java
@@ -1,5 +1,6 @@
 package com.dev.http.servlet;
 
+import com.dev.http.dto.UserDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,9 +13,20 @@ import java.io.IOException;
 @WebServlet("/sessions")
 public class SessionServlet extends HttpServlet {
 
+    private static final String USER = "user";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         var session = req.getSession();
-        System.out.println(session.isNew());
+        var user = (UserDto) session.getAttribute(USER);
+        if (user == null) {
+            user = UserDto.builder()
+                    .id(25L)
+                    .mail("test@gmail.com")
+                    .build();
+            session.setAttribute(USER, user);
+        }
+
+        //System.out.println(session.isNew());
     }
 }
