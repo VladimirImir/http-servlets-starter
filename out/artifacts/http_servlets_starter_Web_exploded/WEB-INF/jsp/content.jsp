2a5810e6db5b4ea260352fe1531d698daf19b3b9
VladimirImir <mimimvvv@gmail.com>
2023-08-02 20:47:52 +0300

    HTTP. Servlets. 42. - Expression Language. EL.

--- out/artifacts/http_servlets_starter_Web_exploded/WEB-INF/jsp/content.jsp
+++ out/artifacts/http_servlets_starter_Web_exploded/WEB-INF/jsp/content.jsp
@@ -5,8 +5,18 @@
 </head>
 <body>
 <%@ include file="header.jsp" %>
-<div></div>
-Content. Русский
+<div>
+    <span>Content. Русский</span>
+    <p>Size: ${requestScope.flights.size()}</p>
+    <p>Id: ${requestScope.flights.get(0).id}</p>
+    <p>Id 2: ${requestScope.flights[1].id}</p>
+    <p>Map Id 2: ${sessionScope.flightsMap[1]}</p>
+    <p>JSESSION id: ${cookie["JSESSIONID"].value}, unique identifier</p>
+    <p>Header: ${header["Cookie"]}</p>
+    <p>Param id: ${param.id}</p>
+    <p>Param test: ${param.test}</p>
+    <p>Empty list: ${not empty requestScope.flights}</p>
+</div>
 </body>
 <%@ include file="footer.jsp" %>
 </html>
--- src/com/dev/http/servlet/ContentServlet.java
+++ src/com/dev/http/servlet/ContentServlet.java
@@ -1,5 +1,7 @@
 package com.dev.http.servlet;
 
+import com.dev.http.dto.FlightDto;
+import com.dev.http.service.FlightService;
 import com.dev.http.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,12 +10,23 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
 
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
 
+    private final FlightService flightService = FlightService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<FlightDto> flightDto = flightService.findAll();
+        req.setAttribute("flights", flightDto);
+        req.getSession().setAttribute("flightsMap", flightDto.stream()
+                .collect(toMap(FlightDto::getId, FlightDto::getDescription)));
+
         req.getRequestDispatcher(JspHelper.getPath("content"))
                 .forward(req, resp);
     }
