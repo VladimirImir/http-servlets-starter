7973af268ac46d4479f8fe93a76760b6b0b22281
VladimirImir <mimimvvv@gmail.com>
2023-08-06 21:55:05 +0300

    HTTP. Servlets. 51. - Authentication. Аутентификация.

--- src/com/dev/http/dao/UserDao.java
+++ src/com/dev/http/dao/UserDao.java
@@ -1,5 +1,7 @@
 package com.dev.http.dao;
 
+import com.dev.http.entity.Gender;
+import com.dev.http.entity.Role;
 import com.dev.http.entity.User;
 import com.dev.http.util.ConnectionManager;
 import lombok.SneakyThrows;
@@ -8,6 +10,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,27 +21,10 @@ public class UserDao implements Dao<Integer, User> {
     private static final UserDao INSTANCE = new UserDao();
 
     private static final String SAVE_SQL =
-            "INSERT INTO users(name, birthdate, email, password, role, gender, image) VALUES (?,?,?,?,?,?,?)";
+            "INSERT INTO users(name, birthday, email, password, role, gender, image) VALUES (?,?,?,?,?,?,?)";
+    private static final String GET_BY_EMAIL_AND_PASSWORD_SQL =
+            "SELECT * FROM users WHERE email = ? AND password = ?";
 
-    @Override
-    public List<User> findAll() {
-        return null;
-    }
-
-    @Override
-    public Optional<User> findById(Integer id) {
-        return Optional.empty();
-    }
-
-    @Override
-    public boolean delete(Integer id) {
-        return false;
-    }
-
-    @Override
-    public void update(User entity) {
-
-    }
 
     @Override
     @SneakyThrows
@@ -63,7 +49,57 @@ public class UserDao implements Dao<Integer, User> {
         }
     }
 
+    @SneakyThrows
+    public Optional<User> findByEmailAndPassword(String email, String password) {
+        try (var connection = ConnectionManager.get();
+             var prepareStatement = connection.prepareStatement(GET_BY_EMAIL_AND_PASSWORD_SQL)) {
+            prepareStatement.setString(1, email);
+            prepareStatement.setString(2, password);
+
+            var resultSet = prepareStatement.executeQuery();
+            User user = null;
+            if (resultSet.next()) {
+                user = buildEntity(resultSet);
+            }
+
+            return Optional.ofNullable(user);
+        }
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findById(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        return false;
+    }
+
+    @Override
+    public void update(User entity) {
+
+    }
+
     public static UserDao getInstance() {
         return INSTANCE;
+    }
+
+    private User buildEntity(ResultSet resultSet) throws java.sql.SQLException {
+        return User.builder()
+                .id(resultSet.getObject("id", Integer.class))
+                .name(resultSet.getObject("name", String.class))
+                .birthday(resultSet.getObject("birthday", Date.class).toLocalDate())
+                .image(resultSet.getObject("image", String.class))
+                .email(resultSet.getObject("email", String.class))
+                .password(resultSet.getObject("password", String.class))
+                .role(Role.find(resultSet.getObject("role", String.class)).orElse(null))
+                .gender(Gender.valueOf(resultSet.getObject("gender", String.class)))
+                .build();
     }
 }
--- src/com/dev/http/service/UserService.java
+++ src/com/dev/http/service/UserService.java
@@ -6,10 +6,13 @@ import com.dev.http.dto.UserDto;
 import com.dev.http.entity.User;
 import com.dev.http.exception.ValidationException;
 import com.dev.http.mapper.CreateUserMapper;
+import com.dev.http.mapper.UserMapper;
 import com.dev.http.validator.CreateUserValidator;
 import com.dev.http.validator.ValidationResult;
 import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
+
+import java.util.Optional;
 
 import static lombok.AccessLevel.PRIVATE;
 
@@ -21,7 +24,13 @@ public class UserService {
     private final CreateUserValidator createUserValidator = CreateUserValidator.getInstance();
     private final UserDao userDao = UserDao.getInstance();
     private final CreateUserMapper createUserMapper = CreateUserMapper.getInstance();
+    private final UserMapper userMapper = UserMapper.getInstance();
     private final ImageService imageService = ImageService.getInstance();
+
+    public Optional<UserDto> login(String email, String password) {
+        return userDao.findByEmailAndPassword(email, password)
+                .map(userMapper::mapFrom);
+    }
 
     @SneakyThrows
     public Integer create(CreateUserDto userDto) {
